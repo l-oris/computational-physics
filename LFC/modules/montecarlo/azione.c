ba7475236213b9a945bbed9123f28945f9a8a4de
federicodematteis <97627039+federicodematteis@users.noreply.github.com>
2022-11-13 12:21:13 +0100

    Update azione.c

--- LFC/modules/montecarlo/azione.c
+++ LFC/modules/montecarlo/azione.c
@@ -15,7 +15,7 @@ float Azione()
 float S=0;
 
     for (int i=0; i<N; i++){
-        S  = S + M/2*(xx[i+1]-x[i])^2-M/2*W*W*(xx[i])(xx[i]);
+        S  = S + M/2*(xx[i+1]-x[i])*(xx[i+1]-x[i])-M/2*W*W*(xx[i])*(xx[i]);
     }
 
 return S;
